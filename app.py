ce0b21ce5733ad3c789294f79948cab96579b8c5
wangwiza <76924804+wangwiza@users.noreply.github.com>
2022-09-18 10:13:46 -0400

    created template folder
    added base, header, and index html files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 
@@ -8,10 +8,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def Index():
-    return "Hello Flask Application"
+    return render_template("index.html")
 
 
 
 
-if __name == "__main__":
+if __name__ == "__main__":
     app.run(debug=True)
